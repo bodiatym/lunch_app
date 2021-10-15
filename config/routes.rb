77bff44ab86912d380fe7804816ab3d9271a03a8
bodiatym <bt@leobit.com>
2021-10-15 14:34:08 +0300

    Add user model, home page, login,register, logout pages, gem rspec update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root 'pages#home'
+  get '/home' => 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
