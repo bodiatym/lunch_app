1fa06b98c7fbda866878e877d1956789a21aec54
bodiatym <bt@leobit.com>
2021-11-02 14:21:16 +0200

    Edit role enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum role: { basic: 0, admin: 1 }, _suffix: :role
+  enum role: {
+    basic: BASIC = 'basic',
+    admin: ADMIN = 'admin'
+  }, _suffix: :role
   after_create :build_profile
   has_one :profile, dependent: :destroy, class_name: 'Users::Profile'
 
   private
 
   def build_profile
-    Users::Profile.create(user: self)
+    ::Users::Profile.create(user: self)
   end
 end
