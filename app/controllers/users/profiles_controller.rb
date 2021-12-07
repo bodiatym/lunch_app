35962355091c27bd2f6f24f4eb9d1620e872666f
bodiatym <bt@leobit.com>
2021-12-07 16:11:26 +0200

    add edit profile

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -9,7 +9,7 @@ module Users
     def edit; end
 
     def update
-      if ::ProfileEditService.call(@profile, profile_params)
+      if @profile.update profile_params
         flash[:success] = t '.success'
         redirect_to users_profile_path(@profile)
       else
