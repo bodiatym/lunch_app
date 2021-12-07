38304658f101a423de9dbadca831d42182c3b6b9
bodiatym <bt@leobit.com>
2021-12-07 14:09:12 +0200

    delete bins and fix registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,8 @@
 
 class RegistrationsController < Devise::RegistrationsController
   def create # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+    build_resource(sign_up_params)
+    
     resource.save
     yield resource if block_given?
     if resource.persisted?
