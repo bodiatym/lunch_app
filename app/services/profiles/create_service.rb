6b69f1b1648bc61baefa486b382a82fa26aa3e07
bodiatym <bt@leobit.com>
2021-12-29 08:26:02 +0200

    Edit facde. add delegate

--- app/services/profiles/create_service.rb
+++ app/services/profiles/create_service.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Profiles
-  class CreatorService < ApplicationService
+  class CreateService < ApplicationService
     attr_reader :user
 
     def initialize(user)
