c98b9fd2908c6ad21a056db0e129aa35835f852a
bodiatym <bt@leobit.com>
2022-01-05 11:07:54 +0200

    Small fixes in partial and system spec

--- spec/system/edit_profile_system_spec.rb
+++ spec/system/edit_profile_system_spec.rb
@@ -3,23 +3,31 @@
 require 'rails_helper'
 
 describe 'User edit profile', type: :feature do
-  let(:user)        { create(:user, :with_profile) }
+  let(:user) { create(:user, :with_profile) }
 
   before do
     login_as(user)
 
     visit edit_users_profile_path
+
+    fill_in 'Surname', with: surname
+
+    click_button 'Save'
   end
 
-  it 'shows success message if params correct' do
-    fill_in      'Surname', with: 'Correct'
-    click_button 'Save'
-    expect(page).to have_content('Success')
+  context 'with valid params' do
+    let(:surname) { 'New surname' }
+
+    it 'shows success message' do
+      expect(page).to have_content('Success')
+    end
   end
 
-  it 'shows error message if params incorrect' do
-    fill_in      'Surname', with: 'A'
-    click_button 'Save'
-    expect(page).to have_content('error')
+  context 'with invalid params' do
+    let(:surname) { 'S' }
+
+    it 'shows error message' do
+      expect(page).to have_content('error')
+    end
   end
 end
