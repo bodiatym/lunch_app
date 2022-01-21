6057979b429ef17b6e41065b087910578471eb92
bodiatym <bt@leobit.com>
2022-01-21 12:42:54 +0200

    Remove delegate

--- app/facades/menus/show_facade.rb
+++ app/facades/menus/show_facade.rb
@@ -4,8 +4,7 @@ module Menus
   class ShowFacade
     attr_reader :menu
 
-    delegate :day_name, :items, to: :menu, prefix: :menu
-    delegate :main_course, :first_course, :drink_course, to: :menu_items, prefix: :menu
+    delegate :day_name, to: :menu, prefix: :menu
 
     def initialize(menu)
       @menu = menu.decorate
