c6b6a203ba7eb51194177afb4ebff534e81b8c42
bodiatym <bt@leobit.com>
2022-01-26 17:17:03 +0200

    Add system spec

--- spec/factories/menu_factory.rb
+++ spec/factories/menu_factory.rb
@@ -3,5 +3,11 @@
 FactoryBot.define do
   factory :menu do
     date { Date.current }
+
+    trait :with_items do
+      after(:build) do |menu|
+        menu.items << FactoryBot.build(:item, :first_course)
+      end
+    end
   end
 end
